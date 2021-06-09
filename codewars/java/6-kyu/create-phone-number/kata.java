56f8f5339281774e47a5e5c88ae4e9679c42d239
Michael Carey <contact@michaelcarey.com.au>
2021-06-09 16:14:28 +1000

    Add 6-kyu Java kata: Create Phone Number

---
+++ codewars/java/6-kyu/create-phone-number/kata.java
@@ -0,0 +1,7 @@
+import java.util.stream.IntStream;
+
+class Kata {
+    public static String createPhoneNumber(int[] numbers) {
+        return String.format("(%d%d%d) %d%d%d-%d%d%d%d", IntStream.of(numbers).boxed().toArray());
+    }
+}

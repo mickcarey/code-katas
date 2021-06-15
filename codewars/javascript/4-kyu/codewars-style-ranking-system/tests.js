4b81d55d96b9ea627a26345bf9d76565117183a0
Michael Carey <contact@michaelcarey.com.au>
2021-06-15 14:49:39 +1000

    Update 4-kyu Javascript kata test function to arrow notation: Codewars Style Ranking System

--- codewars/javascript/4-kyu/codewars-style-ranking-system/tests.js
+++ codewars/javascript/4-kyu/codewars-style-ranking-system/tests.js
@@ -6,14 +6,14 @@ Test cases below as none originally provided in CodeWars console during developm
 const chai = require("chai");
 const assert = chai.assert;
 
-describe("Testing class access", function() {
+describe("Testing class access", () => {
   let user;
   
   before(() => {
     user = new User();
   });
   
-  it("ensure objects have own values", function() {
+  it("ensure objects have own values", () => {
     let userB = new User();
     userB.incProgress(-6);
     
@@ -22,14 +22,14 @@ describe("Testing class access", function() {
     assert.equal(userB.progress, 40);
   });
   
-  it("should not be able to directly modify rank and progress", function() {
+  it("should not be able to directly modify rank and progress", () => {
     assert.throws(() => { user.rank = 8 });
     assert.throws(() => { user._rank = 8 });
     assert.throws(() => { user.progress = 99 });
     assert.throws(() => { user._progress = 99 });
   });
   
-  it("should not be able to access internal methods or properties", function() {
+  it("should not be able to access internal methods or properties", () => {
     assert.throws(() => { user.getNewRank(user.rank, 1); });
     assert.throws(() => { user.calculatePoints(user.rank, 8); });
     assert.throws(() => { user.updateValue({}, "_progress", () => { this._progress = 9; }); });
@@ -37,36 +37,36 @@ describe("Testing class access", function() {
   });
 });
 
-describe("Rank System Test Suite", function() {
+describe("Rank System Test Suite", () => {
   
-  describe("when creating a new user", function() {
+  describe("when creating a new user", () => {
     let user;
     
     before(() => {
       user = new User();
     });
     
-    it("the default rank and progress should be correct", function() {
+    it("the default rank and progress should be correct", () => {
       assert.equal(user.rank, -8);
       assert.equal(user.progress, 0);
     });
     
-    it("they are awarded the correct amount of points for completing a same rank activity", function() {
+    it("they are awarded the correct amount of points for completing a same rank activity", () => {
       user.incProgress(-8);
       assert.equal(user.progress, 3);
     });
     
-    it("they are awarded the correct amount of points for completing an activity 1 rank higher", function() {
+    it("they are awarded the correct amount of points for completing an activity 1 rank higher", () => {
       user.incProgress(-7);
       assert.equal(user.progress, 10);
     });
     
-    it("they are awarded the correct amount of points for completing an activity 3 ranks higher", function() {
+    it("they are awarded the correct amount of points for completing an activity 3 ranks higher", () => {
       user.incProgress(-5);
       assert.equal(user.progress, 90);
     });
     
-    it("they are correctly moved up 1 rank after exceeding progress threshold", function() {
+    it("they are correctly moved up 1 rank after exceeding progress threshold", () => {
       user.incProgress(-6);
       user.incProgress(-6);
       user.incProgress(-6);
@@ -78,7 +78,7 @@ describe("Rank System Test Suite", function() {
       assert.notEqual(user.progress, 0);
     });
     
-    it("they are correctly moved up 2 ranks after exceeding progress threshold", function() {
+    it("they are correctly moved up 2 ranks after exceeding progress threshold", () => {
       user.incProgress(-3);
       
       assert.equal(user.rank, -6);
@@ -88,7 +88,7 @@ describe("Rank System Test Suite", function() {
       assert.notEqual(user.progress, 0);
     });
     
-    it("they are not allowed to complete an invalid ranked activity", function() {
+    it("they are not allowed to complete an invalid ranked activity", () => {
       assert.throws(() => { user.incProgress(9); });
       assert.throws(() => { user.incProgress(0); });
       assert.throws(() => { user.incProgress(-9); });
@@ -96,7 +96,7 @@ describe("Rank System Test Suite", function() {
     
   });
   
-  describe("when a user has reached the maximum rank", function() {
+  describe("when a user has reached the maximum rank", () => {
     let user;
     
     before(() => {
@@ -104,14 +104,14 @@ describe("Rank System Test Suite", function() {
       user.incProgress(8); // Rules of Kata allow direct levelling to 8 if new user completes a single rank 8 activity
     });
     
-    it("the progress score is not updated and rank remains the same", function() {
+    it("the progress score is not updated and rank remains the same", () => {
       user.incProgress(8);
       assert.equal(user.progress, 0);
       assert.equal(user.rank, 8);
     });
   });
   
-  describe("given a -1 ranked user", function() {
+  describe("given a -1 ranked user", () => {
     let user;
     
     before(() => {
@@ -123,23 +123,23 @@ describe("Rank System Test Suite", function() {
       user.incProgress(-1);
     });
     
-    it("when progressing to the next rank they go to rank 1", function() {
+    it("when progressing to the next rank they go to rank 1", () => {
       user.incProgress(4);
       assert.equal(user.rank, 1);
       assert.notEqual(user.rank, -1);
     });
     
-    it("when completing a same ranked activity they receive 3 progress points", function() {
+    it("when completing a same ranked activity they receive 3 progress points", () => {
       user.incProgress(-1);
       assert.equal(user.progress, 3);
     });
     
-    it("when completing a -2 ranked activity they receive 1 progress points", function() {
+    it("when completing a -2 ranked activity they receive 1 progress points", () => {
       user.incProgress(-2);
       assert.equal(user.progress, 1);
     });
     
-    it("when completing a -3 ranked activity they receive 0 progress points", function() {
+    it("when completing a -3 ranked activity they receive 0 progress points", () => {
       user.incProgress(-3);
       assert.equal(user.progress, 0);
     });
